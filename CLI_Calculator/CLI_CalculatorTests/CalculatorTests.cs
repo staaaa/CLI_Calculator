c6acb73f5b77091a59a6aa93d1a9f89dafc1ab6c
Stanisław Bożych <stachubozych@gmail.com>
2022-03-17 23:53:35 +0100

    Added unit tests
    Added unit tests to various calculator's functions.

---
+++ CLI_Calculator/CLI_CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Kalkulator_konsola;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CLI_CalculatorTests
+{
+    public class Tests
+    {
+        Calculator c1 = new Calculator();
+
+        [TestCase ("arg x 5","arg x","x = 5\n")]
+        [TestCase ("arg y 70","arg y","y = 70\n")]
+        [TestCase ("arg a -9","arg a","a = -9\n")]
+        [TestCase ("arg z 5.2", "arg z","z = 5.2\n")]
+        public void Adding_Arguments(string input, string input2, string expectedValue)
+        {
+            //arrange
+            var stringWriter = new StringWriter();
+
+            //act
+            c1.SetInput(input);
+            c1.Evaluate();
+            Console.SetOut(stringWriter);
+            c1.SetInput(input2);
+            c1.Evaluate();
+
+            //assert
+            var output = stringWriter.ToString();
+            Assert.AreEqual(expectedValue, output);
+        }
+        [TestCase ("calc 5+10","15\n")]
+        [TestCase ("= 5+10","15\n")]
+        public void Calculating_Equations(string input, string expectedValue)
+        {
+            //arrange
+            var stringWriter = new StringWriter();
+
+            //act
+            c1.SetInput(input);
+            Console.SetOut(stringWriter);
+            c1.Evaluate();
+            
+
+            //asert
+            var output = stringWriter.ToString();
+            Assert.AreEqual(expectedValue, output);
+        }
+        [TestCase ("arg x 25","arg y 30","args clear")]
+        public void Dropping_All_Arguments(string input, string input2, string input3)
+        {
+            //act
+            c1.SetInput(input);
+            c1.Evaluate();
+            c1.SetInput(input2);
+            c1.Evaluate();
+            c1.SetInput(input3);
+            c1.Evaluate();
+
+            //assert
+            Assert.IsTrue(c1.GetDictArgs().Count == 0);
+        }
+    }
+}
